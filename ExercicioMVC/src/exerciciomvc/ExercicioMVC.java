e629b41c4b8e265bad2d2d5a1cfb2685155cfc27
kK4ssio <diasmonteirokassio@gmail.com>
2024-09-10 17:18:17 -0300

    v1

---
+++ ExercicioMVC/src/exerciciomvc/ExercicioMVC.java
@@ -0,0 +1,12 @@
+//Aplicacao usando o padrao MVC (model.view.controller)
+package exerciciomvc;
+/**
+ * @author Kassio Dias Monteiro
+ */
+public class ExercicioMVC {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
